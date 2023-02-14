e36e85aa25797cfc0b619222056a9d3bd6320edf
Sonicv1 <yunosirham88@gmail.com>
2023-02-15 04:11:13 +0800

    improve code and ui for redeem reward

--- Nvyro/Pages/User/Rewards.cshtml.cs
+++ Nvyro/Pages/User/Rewards.cshtml.cs
@@ -12,19 +12,20 @@ namespace Nvyro.Pages
     public class RewardsAddModel : PageModel
     {
         private readonly RewardService _rewardService;
-
+        private readonly EmailSender _emailSender;
         private readonly UserManager<ApplicationUser> _userManager;
-        public RewardsAddModel(RewardService rewardService, UserManager<ApplicationUser> userManager)
+        public RewardsAddModel(RewardService rewardService, UserManager<ApplicationUser> userManager, EmailSender emailSender)
         {
             _rewardService = rewardService;
             _userManager = userManager;
+            _emailSender = emailSender;
         }
         public List<Reward> RewardsList { get; set; } = new();
         [BindProperty]
         public Reward Rewards { get; set; } = new();
 
         [BindProperty]
-        public UpdateUserModel AppUser { get; set; } = new UpdateUserModel();
+        public ApplicationUser AppUser { get; set; } = new ApplicationUser();
 
         public async Task OnGetAsync()
         {
@@ -33,16 +34,12 @@ namespace Nvyro.Pages
 
             Console.WriteLine(existingUser.Points);
 
-            existingUser.Points = 100;
-
-            AppUser.Points = existingUser.Points;
-
 
             RewardsList = _rewardService.GetAll();
 
 
         }
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPost()
         {
             Console.WriteLine("Redeem Activate 1.0");
             Console.WriteLine("Current Points : {0}", AppUser.Points);
@@ -59,11 +56,36 @@ namespace Nvyro.Pages
                 Rewards.RewardDescription = reward.RewardDescription;
                 Rewards.requiredPoints = reward.requiredPoints;
                 Rewards.RewardPicURL = reward.RewardPicURL;
-                
+
+                int newPoints = int.Parse(reward.requiredPoints);
+
+                var updatedUser = await _userManager.FindByNameAsync(User.Identity.Name);
+
+                updatedUser.Points -= newPoints;
+
+                _userManager.UpdateAsync(updatedUser);
 
                 int newQuantity = int.Parse(reward.availableQuantity) - 1;
 
-                if(newQuantity > 0)
+
+                var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+                var codechars = new char[25];
+                var rnd = new Random();
+
+                for (int i = 0; i < codechars.Length; i++)
+                {
+                    codechars[i] = chars[rnd.Next(chars.Length)];
+                }
+
+                var finalcode = new String(codechars);
+
+                Console.WriteLine(finalcode);
+
+
+                var sendCode = await _emailSender.SendEmailAsync(updatedUser.Email, $"Redemption Code for {Rewards.RewardName} by {updatedUser.UserName}", $"The code for your {Rewards.RewardName} reward is {finalcode}. Enjoy your reward!");
+
+
+                if (newQuantity > 0)
                 {
                     Rewards.availableQuantity = newQuantity.ToString();
 
